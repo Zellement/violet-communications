663c6beb1845bf2d9564541dd655cf1952dc267b
Dan Farrow <dan@zellement.com>
2022-05-07 16:00:16 +0100

    fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/fabvicon.png`,
+        icon: `src/images/favicon.png`,
       },
     },
     {
