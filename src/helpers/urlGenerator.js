7bf5860809b6fe4397afcfc1a66b6ab4ce0b44c8
Dan Farrow <dan@zellement.com>
2023-01-22 16:30:47 +0000

    adjust url helper

--- src/helpers/urlGenerator.js
+++ src/helpers/urlGenerator.js
@@ -1,5 +1,7 @@
 export default function UrlGenerator(props) {
   if (props === "article") {
     return "/news/";
+  } else if (props === "servicePage") {
+    return "/services/";
   } else return null;
 }
